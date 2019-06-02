4d2dc5a3df65e900a78c63bcae8b2d87e7238bff
uiuihby <1350285537@qq.com>
2019-06-02 22:18:21 +0800

    ~

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var XMLJS = require('xml2js');
 //解析，将xml解析为json
 var parser = new XMLJS.Parser();
 //重组，将json重组为xml
-
-
+var fs= require("fs");            // 
+ 
 var EventFunction=require("./wechat")
 var info = {                      //  验证信息
     token: 'test',                //  your wechat token
@@ -26,24 +26,60 @@ function getAccessToken(url){
     request(url, function (error, response, body) {         // 发送请求获取access_token
         let data=JSON.parse(body);
         info.access_token=data.access_token
+        fs.writeFile('./token.txt', data.access_token , function(err) {});   // 将自己的token写入方便查看
       });
 }
 getAccessToken(url);
 // 获取access_token
-
-
-
-
-
-app.post('/', function(req, res, next) {
-
-//获取xml数据
-        req.on("data", function(data) {
-            //将xml解析
-            parser.parseString(data.toString(), function(err, result) {     // xml转字符串
+let menuList={
+    "button": [
+        {
+            "type": "click", 
+            "name": "今日歌曲22", 
+            "key": "V1001_TODAY_MUSIC"
+        }, 
+        {
+            "name": "菜单22", 
+            "sub_button": [
+                {
+                    "type": "view", 
+                    "name": "搜索22", 
+                    "url": "http://www.soso.com/"
+                }, 
+                {
+                    "type": "miniprogram", 
+                    "name": "wx22a", 
+                    "url": "http://mp.weixin.qq.com", 
+                    "appid": "wx286b93c14bbf93aa", 
+                    "pagepath": "pages/lunar/index"
+                }, 
+                {
+                    "type": "click", 
+                    "name": "赞一下我们", 
+                    "key": "V1001_GOOD"
+                }
+            ]
+        }
+    ]
+}
+request({                                                                                            // 发送请求
+    url:' https://api.weixin.qq.com/cgi-bin/menu/create?access_token='+info.access_token,            // 请求路径等info.accesstoken有值在用
+    method: "POST",                            // 请求方式，默认为get
+    headers: {                                 //设置请求头
+        "content-type": "application/json",
+    },
+    body: JSON.stringify(menuList)             // post参数字符串
+}, function(error, response, body) {
+            console.log(body);
+            // console.log(body);
+});
+// 自定义菜单
+app.post('/', function(req, res, next) {                                 // 接收请求，获取xml数据 对请求进行各种回馈
+        req.on("data", function(data) {                                  // 将xml解析
+            parser.parseString(data.toString(), function(err, result) {  // xml转字符串
                 var body = result.xml;
-                console.log(body);
-                var messageType = body.MsgType[0];                      // 获取返回类型
+                console.log(body);                                       // 获取到的body值
+                var messageType = body.MsgType[0];                       // 获取返回类型
                 if(messageType === 'event') {
                     var eventName = body.Event[0];
                     (EventFunction[eventName]||function(){})(body, req, res);
@@ -51,12 +87,10 @@ app.post('/', function(req, res, next) {
                 }else if(messageType === 'text') {
                     EventFunction.responseNews(body, res);
                 }else {
-                    res.send(echostr);
+                    res.send('我好像无法理解您在说什么呢');
                 }
             });
         });
-
-     
 });
  
 
