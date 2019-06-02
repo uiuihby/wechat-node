df3b739479382e4f9b3c96b1e37f0eee24d3349e
uiuihby <1350285537@qq.com>
2019-06-02 10:30:46 +0800

    提交信息

---
+++ app.js
@@ -0,0 +1,73 @@
+//express_demo.js 文件
+var express = require('express');
+var sha1 = require('sha1');       //  秘要加密
+var app = express();
+var request = require('request'); // http请求
+//xml解析模块
+var XMLJS = require('xml2js');
+//解析，将xml解析为json
+var parser = new XMLJS.Parser();
+//重组，将json重组为xml
+
+
+var EventFunction=require("./wechat")
+var info = {                      //  验证信息
+    token: 'test',                //  your wechat token
+    appid: 'wxac382a686f71ef96',  //  your wechat appid
+    appsecret:'6cbe254818a1e4bad3863697b9db4e58',
+    access_token:null,             // 每两小时发送的token
+};
+
+let url="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+info.appid+"&secret="+info.appsecret+""
+setInterval(function(){
+    getAccessToken(url);
+},7000000)
+function getAccessToken(url){
+    request(url, function (error, response, body) {         // 发送请求获取access_token
+        let data=JSON.parse(body);
+        info.access_token=data.access_token
+      });
+}
+getAccessToken(url);
+// 获取access_token
+
+
+
+
+
+app.post('/', function(req, res, next) {
+
+//获取xml数据
+        req.on("data", function(data) {
+            //将xml解析
+            parser.parseString(data.toString(), function(err, result) {     // xml转字符串
+                var body = result.xml;
+                console.log(body);
+                var messageType = body.MsgType[0];                      // 获取返回类型
+                if(messageType === 'event') {
+                    var eventName = body.Event[0];
+                    (EventFunction[eventName]||function(){})(body, req, res);
+                //自动回复消息
+                }else if(messageType === 'text') {
+                    EventFunction.responseNews(body, res);
+                }else {
+                    res.send(echostr);
+                }
+            });
+        });
+
+     
+});
+ 
+
+
+
+// 验证判断
+var server = app.listen(1234, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
---
+++ wechat.js
@@ -0,0 +1,40 @@
+var XMLJS = require('xml2js');
+var builder = new XMLJS.Builder();
+//微信客户端各类回调用接口
+var EventFunction = {
+  //关注
+  subscribe: function(result, req, res) {
+      console.log("关注");  
+  },
+  //注销
+  unsubscribe: function(openid, req, res) {
+      console.log("删除")
+  },
+  //打开某个网页
+  VIEW: function() {
+//根据需求，处理不同的业务
+  },
+//自动回复
+  responseNews: function(body, res) {
+//组装微信需要的json
+      var xml  = {xml: {
+          ToUserName: body.FromUserName,
+          FromUserName: body.ToUserName,
+          CreateTime: + new Date(),
+          MsgType: 'text',
+          Content: '编辑@+您想说的话，我们可以收到'
+      }};
+      var reciviMessage = body.Content[0]
+      if(/^\@.*/.test(reciviMessage)) {
+          xml.xml.Content = '已经收到您的建议，会及时处理！'
+      }
+      if(reciviMessage.indexOf("陈良峰")!=-1){
+          xml.xml.Content = '你好，帅气的开发大大'
+      }
+      xml = builder.buildObject(xml);         // json转为xml
+      res.send(xml);
+  }
+}
+
+module.exports = EventFunction;
+
